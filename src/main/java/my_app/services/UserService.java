22ba228d1c36e650c78d15c6432d9fa18b8ada30
Alex Sahaidachnyi <me@home>
2020-09-13 10:40:24 +0300

    refactored: userService and UserController

--- src/main/java/my_app/services/UserService.java
+++ src/main/java/my_app/services/UserService.java
@@ -1,18 +1,44 @@
 package my_app.services;
 
+import my_app.entities.Role;
+import my_app.entities.User;
 import my_app.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, Map<String,String> myForm, String username){
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+
+        for(String key : myForm.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
